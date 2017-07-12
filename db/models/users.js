7336dacff05a4409aff67aab8ee8c7661244bcbe
Eric Mai <enmai1988@gmail.com>
2017-07-12 11:27:16 -0700

    modified db schema

--- db/models/users.js
+++ db/models/users.js
@@ -1,10 +1,12 @@
 const db = require('../');
 
+/*
 const Profile = db.Model.extend({
-  // tableName: 'profiles',
-  // auths: function() {
-  //   return this.hasMany('Auth');
-  // }
+  tableName: 'profiles',
+  auths: function() {
+    return this.hasMany('Auth');
+  }
 });
 
 module.exports = db.model('Profile', Profile);
+*/
