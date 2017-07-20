d8d2fb8e5e70ec3c06a286d5679728b52597f364
Karina Dalca <karinadalca@gmail.com>
2017-07-20 14:35:50 -0700

    updated database and created dummy data

--- db/index.js
+++ db/index.js
@@ -1,4 +1,7 @@
 const Sequelize = require('sequelize');
+const users = require('./users.json');
+const projects = require('./projects.json');
+const interests = require('./interests.json');
 
 let db = null;
 if (process.env.DATABASE_URL) {
@@ -31,15 +34,16 @@ const Project = db.define('project', {
   companyName: Sequelize.TEXT,
   appName: Sequelize.TEXT,
   byline: Sequelize.TEXT,
-  companyName: Sequelize.TEXT,
+  logo: Sequelize.TEXT,
+  imageURL: Sequelize.TEXT,
   location: Sequelize.TEXT,
   description: Sequelize.TEXT,
   coFounders: Sequelize.TEXT,
   url: Sequelize.TEXT,
-  imageURL: Sequelize.TEXT,
-  currency: Sequelize.TEXT,
   fundedAmount: Sequelize.DECIMAL(10, 2),
-  fundingGoal: Sequelize.DECIMAL(10, 2),
+  neededFunding: Sequelize.DECIMAL(10, 2),
+  status: Sequelize.ENUM('ready', 'creating', 'failed'),
+  categories: Sequelize.TEXT
 });
 
 const Interest = db.define('interest', {
@@ -63,25 +67,13 @@ const Funding = db.define('funding', {
   amount: Sequelize.DECIMAL(10, 2)
 });
 
-// const Image = db.define('image', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   small: Sequelize.TEXT,
-//   full: Sequelize.TEXT
-// });
-
-
 User.hasMany(Project, { foreignKey: 'userId'});
 
 Project.belongsTo(User);
 
 Project.hasMany(Funding, { foreignKey: 'projectId' });
 
-// Project.hasMany(Image, { foreignKey: 'projectId' });
+//Project.hasMany(Image, { foreignKey: 'projectId' });
 
 Funding.belongsTo(User);
 
@@ -91,14 +83,27 @@ Interest.belongsToMany(User, { through: 'UserInterest' });
 
 Interest.belongsToMany(Project, { through: 'ProjectInterest' });
 
-// Image.belongsTo(Project);
-
 // User.hasMany(Interest);
 
 // User.hasMany(Funding);
 
 // Project.hasMany(Interest, { foreignKey: 'projectId' });
 
-// db.sync({ force: true });
+
+/***********************************************
+NOTE: 1. run 'createdb techstarter to create database'
+      2. run 'initdb techstarter' to get log command
+      3. copy paste command returned from above to start server
+      4. run 'node db/index.js' with all bulkCreates commented and sync uncommented first, 
+      5. then run same command with bulkcreate uncommented and sync commented, this will populate database.
+      6. run database with: psql techstarter.
+************************************************/
+
+User.bulkCreate(users);
+Project.bulkCreate(projects);
+Interest.bulkCreate(interests);  
+// db.sync({force: true});
 
 module.exports = { db, User, Project, Interest, Funding };
+
+//https://media.alvinology.com/uploads/2017/04/Fitbit-Alta-HR_Family-1280x640.jpg, http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/2x/b4/p02xb4tc.jpg, http://inphantry.com/wp-content/uploads/2017/06/scorpio-hero.jpg, https://icgtechnology.com/wp-content/uploads/2017/04/TSR-Page-Cloud-Evaluation.jpg, https://monstergeek.uk/wp-content/uploads/2016/10/CyberSecurity-1280x640.jpg, http://68.media.tumblr.com/6d351c2efb19c7d9f2b1d74ca6db1762/tumblr_np9wbjGa1M1tf70vho1_1280.png, http://realtrends.com/blog/wp-content/uploads/2017/02/AdobeStock_113286249-1280x640.jpeg, http://realtrends.com/blog/wp-content/uploads/2017/01/AdobeStock_94353427-1280x640.jpeg, http://realtrends.com/blog/wp-content/uploads/2017/03/AdobeStock_78180349-1-1280x640.jpeg, https://financialtribune.com/sites/default/files/field/image/october/11_nissan_520.jpg, https://i1.wp.com/www.juku.it/wp-content/uploads/2016/10/efficiency.jpeg?resize=1280%2C640, https://cdn-images-1.medium.com/max/1600/1*hoowIZWyQhh5GMOJ7uXfRw.jpeg
