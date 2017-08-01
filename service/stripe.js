05f6c64ee637dd5f8de1a266d033ede72aba337b
Karina Dalca <karinadalca@gmail.com>
2017-08-01 16:11:56 -0700

    gettng user and prject info from front end

--- service/stripe.js
+++ service/stripe.js
@@ -3,11 +3,19 @@ const stripe = require('stripe')(config.secret_key);
 
 module.exports.charge = (req, res) => {
   console.log('incoming payment request: ', req.body);
-  let amount = req.body.amount || 1000;
-  let email = req.body.stripeEmail || 'abc@gmail.com';
-  let source = req.body.stripeToken;
+  let temp = req.headers.referer.split('http://localhost:3000/projects/');
+  let temp2 = temp[1].split('/');
+  projectId = temp2[0];
+
+  let userId = req.user.id;
+  let amount = parseFloat(req.body.amount) || 1000;
+  let source = req.body.source;
   let description = req.body.description || 'test';
   let currency = req.body.currency || 'usd';
+
+  console.log('PORJECTID: ', projectId);
+  console.log('USERID: ', userId);
+  console.log('AMOUNT: ', amount);
 
   stripe.customers.create({ email, source })
     .then(customer => {
