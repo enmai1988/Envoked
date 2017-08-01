b105de19ef69a16aedba51c0088132c827183e5c
Karina Dalca <karinadalca@gmail.com>
2017-08-01 16:25:37 -0700

    send charge to stripe

--- service/stripe.js
+++ service/stripe.js
@@ -1,34 +1,31 @@
 const config = require('config')['stripe'];
 const stripe = require('stripe')(config.secret_key);
+const Sequelize = require('sequelize');
+const { Project, User, Funding } = require('../db');
 
 module.exports.charge = (req, res) => {
-  console.log('incoming payment request: ', req.body);
-  let temp = req.headers.referer.split('http://localhost:3000/projects/');
-  let temp2 = temp[1].split('/');
-  projectId = temp2[0];
+  //console.log('incoming payment request: ', req.body);
+  //let temp = req.headers.referer.split('http://localhost:3000/projects/');
+  //let temp2 = temp[1].split('/');
+
+  let projectId = req.body.projectId;
+  let projectFunded = req.body.projectFunded;
 
   let userId = req.user.id;
   let amount = parseFloat(req.body.amount) || 1000;
+  let stripeAmount = amount*100;
   let source = req.body.source;
   let description = req.body.description || 'test';
   let currency = req.body.currency || 'usd';
 
-  console.log('PORJECTID: ', projectId);
-  console.log('USERID: ', userId);
-  console.log('AMOUNT: ', amount);
-
-  stripe.customers.create({ email, source })
-    .then(customer => {
-      console.log('stripe: ', customer);
-      return stripe.charges.create({
-        amount,
-        currency,
-        description,
-        customer: customer.id
-      });
-    })
+  stripe.charges.create({
+    amount: stripeAmount,
+    currency,
+    description,
+    source,
+    capture: true
+  })
     .then(charge => {
-      console.log('charge: ', charge);
-      res.sendStatus(200);
+      console.log(charge);
     });
 };
