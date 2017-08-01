8cce08ae7a94021be2095581f9aabf7d76234084
Karina Dalca <karinadalca@gmail.com>
2017-08-01 12:33:48 -0700

    added bootstrap modal for payment

--- client/src/containers/projectPage.jsx
+++ client/src/containers/projectPage.jsx
@@ -38,22 +38,20 @@ class ProjectPage extends React.Component {
             /> :
             <Spinner style={{ marginTop: '150px' }} />
         }
-        <div class="container">
-          <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#paymentModal">Fund This Porject!</button>
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#paymentModal">Fund This Project!</button>
 
-          <div id="paymentModal" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  <h4 class="modal-title">Payment</h4>
-                </div>
-                <div>
-                  <Payment />
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
+        <div id="paymentModal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Payment</h4>
+              </div>
+              <div>
+                <Payment />
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
