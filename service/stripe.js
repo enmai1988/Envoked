9bf6a53ceebc1b6c69999d81dfb794d4285663a1
Karina Dalca <karinadalca@gmail.com>
2017-08-02 15:25:53 -0700

    handled NaN amount error

--- service/stripe.js
+++ service/stripe.js
@@ -9,61 +9,72 @@ module.exports.charge = (req, res) => {
 
   if (!req.user) {
     // if there is no user, return error to front end
-    res.status(200).send('Please log in to send fund this project');
+    res.status(200).send('Please log in to fund this project');
   } else {
 
     let userId = req.user.id;
-    let amount = parseFloat(req.body.amount) || 1000;
-    let stripeAmount = amount * 100;
-    let source = req.body.source;
-    let description = req.body.description || 'test';
-    let currency = req.body.currency || 'usd';
 
-    stripe.charges.create({
-      amount: stripeAmount,
-      currency,
-      description,
-      source,
-      capture: true
-    })
-      .then(charge => {
-        let status = charge.status;
-        let chargeAmount = charge.amount;
+    if (isNaN(parseInt(req.body.amount))) {
+      res.status(200).send('Please enter a valid amount');
+    } else {
+      let amount = parseFloat(req.body.amount) || 1000;
+      let stripeAmount = amount * 100;
+      let source = req.body.source;
+      let description = req.body.description || 'test';
+      let currency = req.body.currency || 'usd';
+      let result = 'BlUE';
 
-        // Send back success startus if charge is successful
-        if (status === 'succeeded') {
-          let projectTotal = parseFloat(projectFunded) + amount;
-          console.log('TOTAL: ', projectTotal);
-          res.status(200).send('Charge Successful!');
+      stripe.charges.create({
+        amount: stripeAmount,
+        currency,
+        description,
+        source,
+        capture: true
+      })
+        .then(charge => {
+          let status = charge.status;
+          let chargeAmount = charge.amount;
+          console.log('projectId: ', projectId);
+          console.log('PROJECTFunded: ', projectFunded);
+          console.log('USERID: ', userId);
+          console.log('STATUS: ', status);
+          console.log('AMOUNT: ', chargeAmount);
 
-          // Update currentFunding for projectId in Projects Table
-          Project.update({
-            currentFunding: projectTotal
-          }, { where: { id: projectId } } )
-            .then(function () {
-              console.log("Project updated successfully!");
+          // Send back success startus if charge is successful
+          if (status === 'succeeded') {
+            let projectTotal = parseFloat(projectFunded) + amount;
+            console.log('TOTAL: ', projectTotal);
+            res.status(200).send('Charge Successful!');
+
+            // Update currentFunding for projectId in Projects Table
+            Project.update({
+              currentFunding: projectTotal
+            }, { where: { id: projectId } })
+              .then(function () {
+                console.log("Project updated successfully!");
+              })
+              .catch(function (err) {
+                console.log("Project update failed!");
+              });
+
+            // Insert a new entry to Funding table
+            Funding.create({
+              projectId: projectId,
+              userId: userId,
+              amount: projectTotal
             })
-            .catch(function (err) {
-              console.log("Project update failed!");
-            });
+              .then(function () {
+                console.log("Funding updated successfully!");
+              })
+              .catch(function (err) {
+                console.log("Funding update failed!");
+              });
 
-          // Insert a new entry to Funding table
-          Funding.create({
-            projectId: projectId,
-            userId: userId,
-            amount: projectTotal
-          })
-            .then(function () {
-              console.log("Funding updated successfully!");
-            })
-            .catch(function (err) {
-              console.log("Funding update failed!");
-            });
-
-          // Send back error startus if charge is fails
-        } else {
-          res.status(201).send('Charge Failed!');
-        }
-      });
+            // Send back error startus if charge is fails
+          } else {
+            res.status(201).send('Charge Failed!');
+          }
+        });
+    }
   }
 };
